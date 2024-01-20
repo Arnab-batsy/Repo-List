d6d3aeeba6be9ec3b6cba34a4ef72c444a509821
Arnab Kumar Das <78913766+Arnab-batsy@users.noreply.github.com>
2024-01-21 01:49:51 +0530

    Add 10 repositories per page

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 <h1>This is a Github Repositories listing page</h1>
 - Assignment by Fyle
+
+Device must be connected to the internet in order to enable the GitHub API to fetch the required data.
--- index.js
+++ index.js
@@ -1,16 +1,56 @@
-const API= "https://api.github.com/users/";
+const API = "https://api.github.com/users/";
+const perPage = 10;
+let repoArr = [];
+let repoContainer = $(".repos");
 
-const getUserData= async()=>{
-    const user= $("#search-box").val();
-    const fetchData= await fetch(API+user);
-    const userData= await fetchData.json();
+const getUserData = async () => {
+  const user = $("#search-box").val();
+  const fetchData = await fetch(API + user);
+  const userData = await fetchData.json();
+  // console.log(userData);
 
-    $(".url").html(`<p>${userData.url}</p>`);
-    $("#image").html(`<img src=${userData.avatar_url} />`);
-    $("#details").html(`<h1>${userData.name}</h1>
+  if (userData.message === "Not Found") {
+    $(".home-page").html("<p>User not Found</p>");
+    return;
+  }
+  const totalRepos = userData.public_repos;
+  const totalPages = Math.ceil(totalRepos / 10);
+
+  $(".url").html(`<p>${userData.url}</p>`);
+  $("#image").html(`<img src=${userData.avatar_url} />`);
+  $("#details").html(`<h1>${userData.name}</h1>
     <p>${userData.bio}</p>
     <p>Location: ${userData.location}</p>
     <p>Followers: ${userData.followers}</p>`);
 
-}
+  const Repo = await fetch(API + user + "/repos" + `?per_page=${perPage}`);
+  const data = await Repo.json();
+//   console.log(data);
+  displayRepos(data);
+};
 
+function displayRepos(repos) {
+  repoContainer.html("");
+
+  repos.forEach((param) => {
+    const repoElement = $("<div></div>");
+    repoElement.addClass("repository");
+
+    const repoLink = $("<a></a>");
+    repoLink.attr("href", param.html_url);
+    repoLink.attr("target", "_blank");
+    repoLink.html(`<h3>${param.name}</h3>`);
+    repoElement.append(repoLink);
+
+    const description = $("<p></p>");
+    description.text(param.description || "No description available.");
+    repoElement.append(description);
+
+    const languageButton = $("<button></button>");
+    languageButton.text(param.language || "Not specified");
+    repoElement.append(languageButton);
+
+    repoContainer.append(repoElement);
+    repoArr.push(repoElement);
+  });
+}
