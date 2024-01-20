612ce21c2665e335f4641b27096a84ffa03d3893
Arnab Kumar Das <78913766+Arnab-batsy@users.noreply.github.com>
2024-01-20 23:00:02 +0530

    Integrate API, Fetch Data and Implement Search

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-$(document).keypress((event)=>{
-    $("h1").text(event.key);
-})
+const API= "https://api.github.com/users/";
+
+const getUserData= async()=>{
+    const user= $("#search-box").val();
+    const fetchData= await fetch(API+user);
+    const userData= await fetchData.json();
+
+    $(".url").html(`<p>${userData.url}</p>`);
+    $("#image").html(`<img src=${userData.avatar_url} />`);
+    $("#details").html(`<h1>${userData.name}</h1>
+    <p>${userData.bio}</p>
+    <p>Location: ${userData.location}</p>
+    <p>Followers: ${userData.followers}</p>`);
+
+}
+
