a36b56c6cb23997f0e8b8a6080d887a71e8ca335
Arnab Kumar Das <78913766+Arnab-batsy@users.noreply.github.com>
2024-01-21 17:20:43 +0530

    Add pagination

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 const API = "https://api.github.com/users/";
+let currentPage = 1;
 const perPage = 10;
 let repoArr = [];
+let count = 0;
+let totalPages = null;
 let repoContainer = $(".repos");
 
-const getUserData = async () => {
+const getUserData = async (current) => {
   const user = $("#search-box").val();
   const fetchData = await fetch(API + user);
   const userData = await fetchData.json();
   // console.log(userData);
 
-  if (userData.message === "Not Found") {
+  if (!fetchData.ok) {
     $(".home-page").html("<p>User not Found</p>");
+    $(".home-page").css("text-align", "center");
     return;
   }
   const totalRepos = userData.public_repos;
-  const totalPages = Math.ceil(totalRepos / 10);
+  totalPages = Math.ceil(totalRepos / 10);
+  // console.log(totalPages);
 
   $(".url").html(
     `<p><img src="Assets/link.png" height="13px"/> <a href="${userData.html_url}" target="_blank" style="text-decoration: none; color: inherit; cursor: pointer;">${userData.html_url}</a></p>`
@@ -25,9 +30,23 @@ const getUserData = async () => {
     <p><img src="Assets/location-pin.png" height="13px"/> ${userData.location}</p>
     <p>Followers: ${userData.followers}</p>`);
 
-  const Repo = await fetch(API + user + "/repos" + `?per_page=${perPage}`);
+  const Repo = await fetch(
+    API + user + "/repos" + `?per_page=${perPage}&page=${current}`
+  );
+
+  var pageContainer = $(".page-containers");
+  if (!pageContainer.hasClass("show-children")) {
+    pageContainer.addClass("show-children");
+  }
+
   const data = await Repo.json();
-  console.log(data);
+  // console.log(data);
+
+  if (data.message === "Not Found") {
+    repoContainer.html("No repositories found for the user");
+    return;
+  }
+
   displayRepos(data);
   createRequiredPage(totalPages);
 };
@@ -56,4 +75,28 @@ function displayRepos(repos) {
     repoContainer.append(repoElement);
     repoArr.push(repoElement);
   });
+}
+
+function createRequiredPage(totalPage) {
+  const pageShiftContainer = $(".page-shift-container");
+  let num = parseInt(totalPage);
+  // console.log(num);
+  if (count === 0) {
+    for (let i = 1; i <= num; ++i) {
+      const listItem = $("<li></li>");
+      listItem.addClass("list").text(i);
+      pageShiftContainer.append(listItem);
+      listItem.on("click", handleListItemClick);
+    }
+  }
+  count++;
+}
+
+function handleListItemClick(event){
+  const innerText = $(event.target).text();
+  getUserData(innerText);
+  currentPage = innerText;
+
+  $(".list").css("background-color", ""); // Reset background color for all list items
+  $(event.target).css("background-color", "#7f8ff4");
 }
